eac8dadffe4f612077d3da5f0bb0505b1af34d05
EndOFMaster <843698826@qq.com>
2018-09-10 22:03:05 +0800

    修改

--- src/test/java/com/endofmaster/paypal/PayPalClientTest.java
+++ src/test/java/com/endofmaster/paypal/PayPalClientTest.java
@@ -1,3 +1,5 @@
+package com.endofmaster.paypal;
+
 import com.endofmaster.paypal.PayPalClient;
 import com.endofmaster.paypal.RefreshAccessToken;
 import com.endofmaster.paypal.base.GetAccessTokenRequest;
@@ -9,14 +11,17 @@ import org.junit.Test;
 
 public class PayPalClientTest {
 
-    private final static String clientId = "AZhCTq6c3OP-swNcHYVvLra347KY82jYT65TyZ7WNlaTjyHYVDrr4GrzkZ2akHwk9xnAZVJJbcRW5cm0";
-    private final static String secret = "EDE6TrTOICc-yF3oQEbIXiIPxnp3IC2PEzBfCVVIlJMGBDL_zswNXVDusqCl-_8pB6pWGS1RuAhahWE1";
+//    private final static String clientId = "AZhCTq6c3OP-swNcHYVvLra347KY82jYT65TyZ7WNlaTjyHYVDrr4GrzkZ2akHwk9xnAZVJJbcRW5cm0";
+//    private final static String secret = "EDE6TrTOICc-yF3oQEbIXiIPxnp3IC2PEzBfCVVIlJMGBDL_zswNXVDusqCl-_8pB6pWGS1RuAhahWE1";
+    private final static String clientId = "AehCAuJcd6sCLgmOAozgXbkew9LhkRAEj_RKZWoQxInOu7NEzU6wV4BT_Q8tnf7ozR8s3068a72uoK9i";
+    private final static String secret = "EIh6jNAfNHo5SwfKH_TvDsANfS0SyVKOpS9rRlwwgLh0KAEox383RkkQZ7gtSq1Hq5TFD7n980swNyrV";
 
     private final PayPalClient client;
 
     public PayPalClientTest() {
-        this.client = new PayPalClient(clientId, secret, false);
-        new RefreshAccessToken(client);
+        this.client = new PayPalClient(clientId, secret, true);
+        PayPalClient.ACCESS_TOKEN="A21AAGNc1gRmOOvjNUEngvlZxlAhNcBsX9T7ytM8wmGp937OwgYLJU8oPgkhoA6ZLxDmK3PF3yAU6BipF6DDVajjH80fCMttA";
+//        new RefreshAccessToken(client);
     }
 
     @Test
@@ -32,9 +37,8 @@ public class PayPalClientTest {
 
     @Test
     public void createOrder() throws InterruptedException {
-        Thread.sleep(1000);
-        PayPalCreateOrderRequest request = new PayPalCreateOrderRequest(1, "测试商品：北京一日游", "http://127.0.0.1", "http://127.0.0.1");
-        Thread.sleep(2000);
+        PayPalCreateOrderRequest request = new PayPalCreateOrderRequest(100, "测试商品：北京一日游",
+                "https://trade.zoomdu.com/notify/payPal/charge", "https://trade.zoomdu.com/notify/payPal/charge");
         PayPalCreateOrderResponse response = client.execute(request);
         if (response.success()) {
             System.err.println(response.getPayUrl());
